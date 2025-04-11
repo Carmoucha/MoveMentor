d824317e7b2cd3adac64848ab13781713f3eeefc
Carmoucha <carmenderderian@gmail.com>
2025-04-10 22:35:08 -0400

    Keep my version of server.js

--- MoveMentor/backend/server.js
+++ MoveMentor/backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:8081', // frontend port
+  origin: '*', // or specify IPs if you want to restrict
   credentials: true
 }));
 
@@ -21,9 +21,10 @@ app.use('/workouts', workoutsRoute); // increment, decrement, progress
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(8000, () => {
-      console.log('Server running on http://localhost:8000');
+    app.listen(8000, '0.0.0.0', () => {
+      console.log('Server running on http://0.0.0.0:8000');
     });
+    
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
