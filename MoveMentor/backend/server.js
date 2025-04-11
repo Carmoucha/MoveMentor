3ade76a623f6437c194b12e4f69b5777ed70400a
Leon Robert-Hosein <leonrobhosein@hotmail.com>
2025-04-10 22:11:23 -0400

    fixing issues

--- MoveMentor/backend/server.js
+++ MoveMentor/backend/server.js
@@ -21,9 +21,9 @@ app.use('/workouts', workoutsRoute); // increment, decrement, progress
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(8000, () => {
+    app.listen(8000, '0.0.0.0', () => {
       console.log('Server running on http://localhost:8000');
-    });
+    });    
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
